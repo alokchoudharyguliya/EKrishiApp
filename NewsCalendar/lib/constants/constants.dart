7c76ee61e7e0ef42ab9385a85e86c2bc5251c002
Pseudophoenix <waynerooney0089@gmail.com>
2025-09-16 23:08:19 +0530

    CCTV feed remains

--- NewsCalendar/lib/constants/constants.dart
+++ NewsCalendar/lib/constants/constants.dart
@@ -1,2 +1,2 @@
-const String BASE_URL = 'http://10.129.36.15:3000';
-const String SOCK_BASE_URL = 'ws://10.129.36.15:3000';
+const String BASE_URL = 'http://10.95.191.15:3000';
+const String SOCK_BASE_URL = 'ws://10.95.191.15:3000';
